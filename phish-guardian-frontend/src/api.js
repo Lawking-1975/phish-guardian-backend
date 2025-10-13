aaaf8637f1800048eb7ff1fe4a6f49892acbbf29
Lawking <czar1975h@gmail.com>
2025-10-13 22:57:31 +0530

    Ready for Vercel deployment

--- phish-guardian-frontend/src/api.js
+++ phish-guardian-frontend/src/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
 
+
+
 export async function predictUrl(url) {
   const res = await axios.post(`${API_BASE}/predict`, { url });
   return res.data;
